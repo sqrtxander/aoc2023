60a93078d5f11036d3f15c017f5b37ae63f515fd
sqrtxander <xanderjames128@gmail.com>
2023-12-14 09:29:55 +1100

    cleaner haskell

--- day01/part1.hs
+++ day01/part1.hs
@@ -8,17 +8,17 @@ getNums (x:xs)
     | isDigit x = (toInteger . digitToInt) x:getNums xs
     | otherwise = getNums xs
 
-sumFirst :: [Integer] -> Integer
-sumFirst [] = 0
-sumFirst (x:xs) = x
+getFirst :: [Integer] -> Integer
+getFirst [] = 0
+getFirst (x:xs) = x
 
-sumLast :: [Integer] -> Integer
-sumLast [] = 0
-sumLast [x] = x
-sumLast (x:xs) = sumLast(xs)
+getLast :: [Integer] -> Integer
+getLast [] = 0
+getLast [x] = x
+getLast (x:xs) = getLast(xs)
 
 sumFirstLast :: [Integer] -> Integer
-sumFirstLast list = 10 * sumFirst list + sumLast list
+sumFirstLast list = 10 * getFirst list + getLast list
 
 main = do
     input <- readFile "input.in"
