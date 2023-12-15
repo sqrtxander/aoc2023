61db6fc5c07aee103431f6daab3f8ccd33846a00
sqrtxander <xanderjames128@gmail.com>
2023-12-15 15:00:55 +1100

    reformat haskell code

--- day01/part1.hs
+++ day01/part1.hs
@@ -1,21 +1,21 @@
-import System.IO
-import Data.List
 import Data.Char
+import Data.List
+import System.IO
 
 getNums :: String -> [Integer]
 getNums "" = []
-getNums (x:xs)
-    | isDigit x = (toInteger . digitToInt) x:getNums xs
+getNums (x : xs)
+    | isDigit x = (toInteger . digitToInt) x : getNums xs
     | otherwise = getNums xs
 
 getFirst :: [Integer] -> Integer
 getFirst [] = 0
-getFirst (x:xs) = x
+getFirst (x : xs) = x
 
 getLast :: [Integer] -> Integer
 getLast [] = 0
 getLast [x] = x
-getLast (x:xs) = getLast(xs)
+getLast (x : xs) = getLast (xs)
 
 sumFirstLast :: [Integer] -> Integer
 sumFirstLast list = 10 * getFirst list + getLast list
@@ -24,4 +24,3 @@ main = do
     input <- readFile "input.in"
     let ls = lines input
     print $ sum $ map (sumFirstLast . getNums) ls
-
--- day01/part2.hs
+++ day01/part2.hs
@@ -1,30 +1,30 @@
-import System.IO
-import Data.List
 import Data.Char
+import Data.List
+import System.IO
 
 getNums :: String -> [Integer]
 getNums "" = []
-getNums (x:xs)
-    | isPrefixOf "one" (x:xs) = 1:getNums xs
-    | isPrefixOf "two" (x:xs) = 2:getNums xs
-    | isPrefixOf "three" (x:xs) = 3:getNums xs
-    | isPrefixOf "four" (x:xs) = 4:getNums xs
-    | isPrefixOf "five" (x:xs) = 5:getNums xs
-    | isPrefixOf "six" (x:xs) = 6:getNums xs
-    | isPrefixOf "seven" (x:xs) = 7:getNums xs
-    | isPrefixOf "eight" (x:xs) = 8:getNums xs
-    | isPrefixOf "nine" (x:xs) = 9:getNums xs
-    | isDigit x = (toInteger . digitToInt) x:getNums xs
+getNums (x : xs)
+    | isPrefixOf "one" (x : xs) = 1 : getNums xs
+    | isPrefixOf "two" (x : xs) = 2 : getNums xs
+    | isPrefixOf "three" (x : xs) = 3 : getNums xs
+    | isPrefixOf "four" (x : xs) = 4 : getNums xs
+    | isPrefixOf "five" (x : xs) = 5 : getNums xs
+    | isPrefixOf "six" (x : xs) = 6 : getNums xs
+    | isPrefixOf "seven" (x : xs) = 7 : getNums xs
+    | isPrefixOf "eight" (x : xs) = 8 : getNums xs
+    | isPrefixOf "nine" (x : xs) = 9 : getNums xs
+    | isDigit x = (toInteger . digitToInt) x : getNums xs
     | otherwise = getNums xs
 
 getFirst :: [Integer] -> Integer
 getFirst [] = 0
-getFirst (x:xs) = x
+getFirst (x : xs) = x
 
 getLast :: [Integer] -> Integer
 getLast [] = 0
 getLast [x] = x
-getLast (x:xs) = getLast(xs)
+getLast (x : xs) = getLast (xs)
 
 sumFirstLast :: [Integer] -> Integer
 sumFirstLast list = 10 * getFirst list + getLast list
@@ -33,4 +33,3 @@ main = do
     input <- readFile "input.in"
     let ls = lines input
     print $ sum $ map (sumFirstLast . getNums) ls
-
